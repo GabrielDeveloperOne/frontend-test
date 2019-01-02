94e246fc67d22fad0ffc16e42091f4e3aae3a91d
Gabriel <gabriel.developer@outlook.com>
2019-01-02 12:32:00 -0200

    primeira requisição JSON

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,10 @@
 // your code
+let fazendeirosJSON = $.getJSON( "../fazenda.json", function() {
+    console.log( "Bem-vindo fazendeiros" );
+  });
+
+  function FaltaDeTempo(){
+      alert("Caro desenvolvedor, infelizmente por falta de tempo só pude concluí parte do teste. Se mesmo assim puder analisar o meu código, ficarei grato")
+  }
+
+
