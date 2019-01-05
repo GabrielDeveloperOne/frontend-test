6498fd5d90228006132229520e887d4815f60230
Gabriel <gabriel.developer@outlook.com>
2019-01-05 17:25:06 -0200

    Adicionando contador de posições

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,25 +9,45 @@
             ordenarFazendeiros.sort(function(a, b) {
           return a.negative - b.negative;
         });
-      
-        for(let piao = 0; piao < data.data.length; piao++){
 
-          if(data.data[piao].positive === null){
-              $(data.data[piao]).remove();
-            } else {
+        /* Adicionando fazendeiros */
+        for(let piao = 0; piao < data.data.length; piao++){
               let nome = data.data[piao].name;
               let description = data.data[piao].description;
               let picture = data.data[piao].picture;
-                    /* Calculo porcentagem positivo e negativo*/
-              let positive = ((data.data[piao].positive / 100000000) * 100).toFixed(2);
-              let negative = ((data.data[piao].negative / 100000000) * 100).toFixed(2);
 
-              let addFazendeiro = '<div class="fazendeiro">'
-              + '<p class="nomeFazendeiro">'
-              + '<img class="fotoFazendeiro" src="' + picture + '">'
-              + '</p>' 
-              + '</div>'
-              
+                    /* Calculo porcentagem positivo e negativo*/
+              let positive = ((data.data[piao].positive / 100000000) * 100).toFixed(0);
+              let negative = ((data.data[piao].negative / 100000000) * 100).toFixed(0);
+              let numeroPosicao = piao + 1;
+                  /* Removendo tooltips de Fazendeiros sem nota */
+          if(data.data[piao].positive === null){
+                let addFazendeiro = '<div class="fazendeiro">'
+                + '<p class="nomeFazendeiro">' + nome + '</p>'
+                + '<p class="contarPosicao">' + numeroPosicao + '</p>'
+                + '<img class="fotoFazendeiro" src="' + picture + '">' 
+                + '</div>'
+                $(".box").append(addFazendeiro);
+            } else {
+                /* inserindo Fazendeiros no Layout */
+                let addFazendeiro = '<div class="fazendeiro">'
+               
+                + '<p class="nomeFazendeiro">' + nome + '</p>'
+                + '<p class="contarPosicao">' + numeroPosicao + '</p>'
+                + '<img class="fotoFazendeiro" src="' + picture + '">'
+                
+                + '<div class="tooltip">'
+                + '<span class="opiniao">'
+                + '<div class="gosta">'
+                + '<span class="estiloGosta">GOSTAM</span>'
+                + '<span class="gostaPorcento">' + positive + '%</span>'
+                + '</div>'
+                + '<div class="naoGosta">'
+                + '<span class="estiloNaoGosta">NÃO GOSTAM</span>'
+                + '<span class="NaoGostaPorcento">' + negative + '%</span>'
+                + '</div>'
+                + '</div>'
+                + '</div>'  
               $(".box").append(addFazendeiro);
             }         
         }     
